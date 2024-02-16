1a09fc50e816bf75b50fc668fa4af3e21283394e
Stefany Borin <stefanyceleste1730@gmail.com>
2024-02-16 19:03:28 -0300

    Implementado rota Delete - rota delete postagem de anúncio

--- src/routes/routesJobs.js
+++ src/routes/routesJobs.js
@@ -7,14 +7,18 @@ const validateSchema = require('../middleware/validateSchema');
 const schemaJob = require('../validations/schemaJob');
 const schemaPostUpdate = require('../validations/schemaPostUpdate');
 const updatePostingJob = require('../controllers/jobs/updatePostingJob');
+const deletePostingJob = require('../controllers/jobs/deletePostingJob');
 
 const routesJobs = Router();
 
 routesJobs.post('/job', validateSchema(schemaJob), createAJobs);
+
 routesJobs.put(
     '/job/:job_id',
     validateSchema(schemaPostUpdate),
     updatePostingJob,
 );
+
+routesJobs.delete('/job/:job_id', deletePostingJob);
 
 module.exports = routesJobs;
