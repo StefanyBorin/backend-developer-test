99feb8826dd672baecc3e151dbb4a17b932a5497
Stefany Borin <stefanyceleste1730@gmail.com>
2024-02-16 15:47:32 -0300

    Adicionado rota put/job/:job_id, validações e controladores rota - editar titulo, description e location

--- src/routes/routesJobs.js
+++ src/routes/routesJobs.js
@@ -5,9 +5,16 @@ const createAJobs = require('../controllers/jobs/createAJob');
 const validateSchema = require('../middleware/validateSchema');
 
 const schemaJob = require('../validations/schemaJob');
+const schemaPostUpdate = require('../validations/schemaPostUpdate');
+const updatePostingJob = require('../controllers/jobs/updatePostingJob');
 
 const routesJobs = Router();
 
 routesJobs.post('/job', validateSchema(schemaJob), createAJobs);
+routesJobs.put(
+    '/job/:job_id',
+    validateSchema(schemaPostUpdate),
+    updatePostingJob,
+);
 
 module.exports = routesJobs;
