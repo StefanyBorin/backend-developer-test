8b107a2e780d3568937b7739f6cc8180ecade0b9
Stefany Borin <stefanyceleste1730@gmail.com>
2024-02-17 13:40:18 -0300

    Implementado controlador updateStatusPublish.js - rota publicar um trabalho

---
+++ src/controllers/jobs/updateStatusPublish.js
@@ -0,0 +1,39 @@
+const {
+    editStatusPosting,
+    fetchPostingById,
+} = require('../../repositories/jobs');
+
+const updateStatusPublish = async (req, res) => {
+    const { job_id } = req.params;
+    const statusPublished = 'published';
+    try {
+        const postingExist = await fetchPostingById(job_id);
+
+        if (!postingExist) {
+            return res.status(400).json({ mensagem: 'Posting does not exist' });
+        }
+
+        const { status } = postingExist;
+
+        if (status.toLowerCase() === statusPublished.toLowerCase()) {
+            res.status(400).json({ message: 'Job already posted' });
+        }
+        const newStatus = await editStatusPosting({
+            id: job_id,
+            status: newStatus,
+        });
+
+        return res.status(201).json(newStatus);
+    } catch (error) {
+        if (
+            error.message.includes(
+                'sintaxe de entrada é inválida para tipo uuid',
+            )
+        ) {
+            return res.status(400).json({ mensagem: 'Posting does not exist' });
+        }
+        return res.status(500).json({ mensagem: 'Internal Server Error' });
+    }
+};
+
+module.exports = updateStatusPublish;
