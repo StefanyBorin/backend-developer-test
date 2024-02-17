2667c1444fa3027052e978fac812fd07c1da9bc7
Stefany Borin <stefanyceleste1730@gmail.com>
2024-02-17 13:47:02 -0300

    Definindo rota PUT /job/:job_id - rota publicar um trabalho

--- src/routes/routesJobs.js
+++ src/routes/routesJobs.js
@@ -1,24 +1,23 @@
 const { Router } = require('express');
 
-const createAJobs = require('../controllers/jobs/createAJob');
-
 const validateSchema = require('../middleware/validateSchema');
-
 const schemaJob = require('../validations/schemaJob');
 const schemaPostUpdate = require('../validations/schemaPostUpdate');
+
+const createAJobs = require('../controllers/jobs/createAJob');
 const updatePostingJob = require('../controllers/jobs/updatePostingJob');
 const deletePostingJob = require('../controllers/jobs/deletePostingJob');
+const updateStatusPublish = require('../controllers/jobs/updateStatusPublish');
 
 const routesJobs = Router();
 
 routesJobs.post('/job', validateSchema(schemaJob), createAJobs);
-
 routesJobs.put(
     '/job/:job_id',
     validateSchema(schemaPostUpdate),
     updatePostingJob,
 );
-
 routesJobs.delete('/job/:job_id', deletePostingJob);
+routesJobs.put('/job/:job_id/publish', updateStatusPublish)
 
 module.exports = routesJobs;
