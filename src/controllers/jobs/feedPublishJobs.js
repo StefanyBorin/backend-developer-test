fe274e6010d0023a498d6813c6f1406fa1645fa1
Stefany Borin <stefanyceleste1730@gmail.com>
2024-02-21 12:09:25 -0300

    Atualizando uma verificação

--- src/controllers/jobs/feedPublishJobs.js
+++ src/controllers/jobs/feedPublishJobs.js
@@ -3,6 +3,10 @@ const { s3, params } = require('../../setting/awsS3');
 const feedPublishJobs = async (req, res) => {
     try {
         const searchObjectS3 = await s3.getObject(params).promise();
+        const { Body } = searchObjectS3;
+        if (!Body) {
+            res.status(400).json({ message: 'Error em buscar feed' });
+        }
         const jsonFormat = JSON.parse(searchObjectS3.Body.toString());
 
         return res.status(200).json(jsonFormat);
